9e2bc112d5bdbec85b899604c40d5a389ae8527a
kairu <kairugaming@gmail.com>
2024-05-02 20:19:42 +0800

    Remove tenant resource

--- crud_resource/__init__.py
+++ crud_resource/__init__.py
@@ -1,6 +1,6 @@
 from .user import UserResource
 from .unit import UnitResource
-from .tenant import TenantResource
+#from .tenant import TenantResource
 from .lease import LeaseAgreementResource
 from .payment import PaymentResource
 from .bill import BillResource
