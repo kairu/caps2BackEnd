b7192476e5a7053343d9495de80b19c694232e0d
kairu <kairugaming@gmail.com>
2024-05-02 20:18:06 +0800

    Add contract saving and OCR saving

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import request
 from creation import app, api, startup, send_from_directory
 # Define Resources
-from crud_resource import UserResource, UnitResource, TenantResource, LeaseAgreementResource, PaymentResource, BillResource, CmsResource, AccessControlResource
+from crud_resource import UserResource, UnitResource, LeaseAgreementResource, PaymentResource, BillResource, CmsResource, AccessControlResource
 import pathlib
 from enums import MIMETYPES
 
 # Add Resources to the API
 api.add_resource(UserResource, '/user', '/user/<string:email_or_user_id>')
 api.add_resource(UnitResource, '/unit', '/unit/<int:unit_id>')
-api.add_resource(TenantResource, '/tenant', '/tenant/<int:tenant_id>')
+# api.add_resource(TenantResource, '/tenant', '/tenant/<int:tenant_id>')
 api.add_resource(LeaseAgreementResource, '/lease', '/lease/<int:lease_agreement_id>')
 api.add_resource(PaymentResource, '/payment', '/payment/<int:payment_id>')
 api.add_resource(BillResource, '/bill', '/bill/<int:bill_id>')
 api.add_resource(CmsResource, '/cms', '/cms/<int:cms_id>')
 api.add_resource(AccessControlResource, '/accesscontrol')
 
+def get_mimetype(filename):
+    return MIMETYPES.get(pathlib.Path(filename.lower()).suffix, 'application/octet-stream')
+
+def hash_filename(filename):
+    hash_filename = hashlib.sha256(filename.encode('utf-8')).hexdigest()
+    hash_filename += pathlib.Path(filename).suffix
+    return hash_filename
 
 # Temporary 
 @app.route('/')
@@ -28,10 +35,12 @@ def index():
 def serve_image(filename):
     if app.config['BULLETIN_IMAGES']:
         return send_from_directory(app.config['BULLETIN_IMAGES'], filename, mimetype=get_mimetype(filename))
-    
-def get_mimetype(filename):
-    return MIMETYPES.get(pathlib.Path(filename.lower()).suffix, 'application/octet-stream')
 
+@app.route('/contract/<filename>', methods=['GET'])
+def serve_contract(filename):
+    if app.config['CONTRACTS']:
+        return send_from_directory(app.config['CONTRACTS'], filename, mimetype=get_mimetype(filename))
+    
 import hashlib
 import os
 @app.route('/bulletin', methods=['POST'])
@@ -39,11 +48,33 @@ def store_bulletin_image():
     if app.config['BULLETIN_IMAGES']:
         if request.method == 'POST':
             file = request.files['file']
-            hash_filename = hashlib.sha256(file.filename.encode('utf-8')).hexdigest()
-            hash_filename += pathlib.Path(file.filename).suffix
+            hash_filename = hash_filename(file.filename)
             filepath = f'{app.config["BULLETIN_IMAGES"]}/{hash_filename}'
             if os.path.exists(filepath):
                 os.remove(filepath)
             file.save(filepath)
             return {'file': hash_filename}, 200
-
+        
+@app.route('/contract/', methods=['POST'])
+def store_contract_image():
+    if app.config['CONTRACTS']:
+        if request.method == 'POST':
+            file = request.files['file']
+            hash_filename = hash_filename(file.filename)
+            filepath = f'{app.config["CONTRACTS"]}/{hash_filename}'
+            if os.path.exists(filepath):
+                os.remove(filepath)
+            file.save(filepath)
+            return {'file': hash_filename}, 200
+        
+@app.route('/OCR', methods=['POST'])
+def ocr_image():
+    if app.config['OCR_RECEIPTS']:
+        if request.method == 'POST':
+            file = request.files['file']
+            hash_filename = hash_filename(file.filename)
+            filepath = f'{app.config["OCR_RECEIPTS"]}/{hash_filename}'
+            if os.path.exists(filepath):
+                os.remove(filepath)
+            file.save(filepath)
+            return {'file': hash_filename}, 200
