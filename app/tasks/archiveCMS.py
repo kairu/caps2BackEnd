a23fd29c7581809ffe3d197fef5f60e6184742b7
kairu <kairugaming@gmail.com>
2024-06-12 03:28:41 +0800

    Replace db.app with current_app

--- app/tasks/archiveCMS.py
+++ app/tasks/archiveCMS.py
@@ -1,14 +1,16 @@
 from ..models import Cms
 from .common import datetime, db
+from flask import current_app
 
 def check_cms_archive():
-    if not Cms.query.all():
-        return
-    current_date = datetime.now().date()
-    cms_list = Cms.query.all()
-    for cms in cms_list:
-        if cms.archive or cms.date_to_end is None:
-            continue
-        if cms.date_to_end < current_date:
-            cms.archive = True
-    db.session.commit()
+    with current_app.app_context():
+        if not Cms.query.all():
+            return
+        current_date = datetime.now().date()
+        cms_list = Cms.query.all()
+        for cms in cms_list:
+            if cms.archive or cms.date_to_end is None:
+                continue
+            if cms.date_to_end < current_date:
+                cms.archive = True
+        db.session.commit()
--- app/tasks/billsDelinquent.py
+++ app/tasks/billsDelinquent.py
@@ -1,13 +1,15 @@
 from ..models import Bill
 from .common import db, datetime
+from flask import current_app
 
 def generate_delinquency():
     current_date = datetime.now().date()
-    if not Bill.query.all():
-        return
-    bills_list = Bill.query.all()
-    for bill in bills_list:
-        if bill.status == 1 and (bill.due_date < current_date):
-            bill.delinquent_amount = (bill.total_amount * 0.03) * ((current_date - bill.due_date).days // 30)
-            # print(f'Bill {bill.bill_id} has been delinquent for {bill.delinquent_amount} since {bill.due_date} of days {(current_date - bill.due_date).days // 30}')
-    db.session.commit()
+    with current_app.app_context():
+        if not Bill.query.all():
+            return
+        bills_list = Bill.query.all()
+        for bill in bills_list:
+            if bill.status == 1 and (bill.due_date < current_date):
+                bill.delinquent_amount = (bill.total_amount * 0.03) * ((current_date - bill.due_date).days // 30)
+                # print(f'Bill {bill.bill_id} has been delinquent for {bill.delinquent_amount} since {bill.due_date} of days {(current_date - bill.due_date).days // 30}')
+        db.session.commit()
