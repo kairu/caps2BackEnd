d204483718a2f1148a41eaa3b376035432e2f41c
kairu <kairugaming@gmail.com>
2024-06-15 00:59:51 +0800

    Add Gunicorn

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run()
+    
+# gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
